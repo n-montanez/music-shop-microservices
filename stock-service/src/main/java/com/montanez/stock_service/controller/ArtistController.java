0279a9db2c5e723ee88a332d91563c9c3b3b2f71
Nicolás Montañez Velasco <nicolas.montanez@globant.com>
2025-03-18 12:26:53 -0500

    feat: using dtos for artist controller response

--- stock-service/src/main/java/com/montanez/stock_service/controller/ArtistController.java
+++ stock-service/src/main/java/com/montanez/stock_service/controller/ArtistController.java
@@ -2,14 +2,16 @@ package com.montanez.stock_service.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.montanez.stock_service.model.artist.Artist;
+import com.montanez.stock_service.model.artist.dto.ArtistDetail;
+import com.montanez.stock_service.model.artist.dto.ArtistSimple;
 import com.montanez.stock_service.service.ArtistService;
 import lombok.RequiredArgsConstructor;
 import java.util.List;
+import java.util.UUID;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/artist")
@@ -18,8 +20,13 @@ public class ArtistController {
     private final ArtistService artistService;
 
     @GetMapping(value = { "/", "" })
-    public ResponseEntity<List<Artist>> getAllArtists() {
-        return new ResponseEntity<>(artistService.getAllArtists(), HttpStatus.OK);
+    public ResponseEntity<List<ArtistSimple>> getAllArtists() {
+        return new ResponseEntity<>(artistService.getAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ArtistDetail> getArtist(@PathVariable UUID id) {
+        return new ResponseEntity<>(artistService.getById(id), HttpStatus.OK);
     }
 
 }
--- stock-service/src/main/java/com/montanez/stock_service/service/ArtistService.java
+++ stock-service/src/main/java/com/montanez/stock_service/service/ArtistService.java
@@ -1,11 +1,16 @@
 package com.montanez.stock_service.service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.stereotype.Service;
 
+import com.montanez.stock_service.model.album.Album;
+import com.montanez.stock_service.model.album.dto.AlbumSimple;
 import com.montanez.stock_service.model.artist.Artist;
+import com.montanez.stock_service.model.artist.dto.ArtistDetail;
+import com.montanez.stock_service.model.artist.dto.ArtistSimple;
 import com.montanez.stock_service.repository.ArtistRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -16,15 +21,41 @@ public class ArtistService {
 
     private final ArtistRepository artistRepository;
 
-    public Artist getSingleArtist(UUID id) {
+    public ArtistDetail getById(UUID id) {
         Optional<Artist> foundArtist = artistRepository.findById(id);
-        if (foundArtist.isPresent())
-            return foundArtist.get();
-        // TODO: Handle 404
+        if (foundArtist.isPresent()) {
+            Artist artist = foundArtist.get();
+            List<AlbumSimple> albumSimples = new ArrayList<>();
+
+            for (Album album : artist.getAlbums()) {
+                albumSimples.add(AlbumSimple.builder()
+                        .id(album.getId())
+                        .title(album.getTitle())
+                        .releaseDate(album.getReleaseDate())
+                        .duration(album.getDuration())
+                        .tracks(album.getTracks())
+                        .build());
+            }
+
+            return ArtistDetail.builder()
+                    .id(artist.getId())
+                    .name(artist.getName())
+                    .albums(albumSimples)
+                    .build();
+        }
         return null;
     }
 
-    public List<Artist> getAllArtists() {
-        return artistRepository.findAll();
+    public List<ArtistSimple> getAll() {
+        List<Artist> artists = artistRepository.findAll();
+        List<ArtistSimple> artistSimples = new ArrayList<>();
+        for (Artist artist : artists) {
+            artistSimples.add(
+                    ArtistSimple.builder()
+                            .name(artist.getName())
+                            .id(artist.getId())
+                            .build());
+        }
+        return artistSimples;
     }
 }
